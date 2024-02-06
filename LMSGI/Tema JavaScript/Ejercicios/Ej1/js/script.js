6a5b2112164b8185dbab1487ce4f9fe50d4b7613
Arcras <manuelcontrerasreyes2005@gmail.com>
2024-02-06 18:20:12 +0100

    Ejercicios JavaScript

---
+++ LMSGI/Tema JavaScript/Ejercicios/Ej1/js/script.js
@@ -0,0 +1,4 @@
+function CamiarColor() {
+    var color = document.getElementById('color').value
+    document.body.style.backgroundColor = color
+}
---
+++ LMSGI/Tema JavaScript/Ejercicios/Ej8.1/js/script.js
@@ -0,0 +1,17 @@
+window.addEven
+
+function CambiarImagen() {
+    var img = document.getElementById('imagen')
+    imagen = new Image()
+    imagen.src = '/img/descarga.png'
+
+    img.src = imagen
+}
+
+function CambiarTitulo() {
+    
+}
+
+function Cambiartexto() {
+    
+}
