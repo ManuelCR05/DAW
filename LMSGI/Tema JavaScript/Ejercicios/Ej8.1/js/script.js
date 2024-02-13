9a8aa4bdef21ad15f8e8dc8fbc01369adfcac70b
Arcras <manuelcontrerasreyes2005@gmail.com>
2024-02-13 18:17:16 +0100

    Modificación del segundo ejercicio
    Modificación del segundo ejercicio de javascript

--- LMSGI/Tema JavaScript/Ejercicios/Ej8.1/js/script.js
+++ LMSGI/Tema JavaScript/Ejercicios/Ej8.1/js/script.js
@@ -1,18 +1,42 @@
-var buttonImg = document.getElementById('cImagen')
-var buttonTitle = document.getElementById('cTitulo')
-var buttonTxt = document.getElementById('cTexto')
+const buttonImg = document.getElementById('cImagen')
+const buttonTitle = document.getElementById('cTitulo')
+const buttonTxt = document.getElementById('cTexto')
+const volverImg = document.getElementById('vImagen')
+const volverTitle = document.getElementById('vTitulo')
+const volverTxt = document.getElementById('vTexto')
 var imag = document.getElementById('imagen')
 var titulo = document.getElementById('titulo')
 var texto = document.getElementById('texto')
+const URLDADOS = 'img/PNG_transparency_demonstration_1.png'
+const URLPICACHU = 'img/descarga.png'
+const TITULO1 = 'Práctica de JavaScript'
+const TITULO2 = 'hola'
+const TXT1 = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. <br>' + 
+             'Deserunt vero doloribus autem numquam ipsum vitae facere. <br>' + 
+             'Labore sit quisquam numquam odit amet nam eveniet. <br>' + 
+             'Laborum nihil aperiam possimus ipsa quasi?'
+const TXT2 = 'si'
 
 buttonImg.addEventListener('click', event => {
-    imag.src = 'img/descarga.png'
+    imag.src = URLPICACHU
 })
 
 buttonTitle.addEventListener('click', event => {
-    titulo.textContent = 'hola'
+    titulo.textContent = TITULO2
 })
 
 buttonTxt.addEventListener('click', event => {
-    texto.textContent = 'si'
+    texto.textContent = TXT2
+})
+
+volverImg.addEventListener('click', event => {
+    imag.src = URLDADOS
+})
+
+volverTitle.addEventListener('click', event => {
+    titulo.textContent = TITULO1
+})
+
+volverTxt.addEventListener('click', event => {
+    texto.innerHTML = TXT1
 })
