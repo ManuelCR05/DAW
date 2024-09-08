22e58a3fec809118aa5cb7bbfad7a4e57a8f282e
Arcras <manuelcontrerasreyes2005@gmail.com>
2024-09-08 23:35:45 +0200

    implementación del DataGridView

--- GestorContrasenyas/GestorContrasenyas/Form1.Designer.cs
+++ GestorContrasenyas/GestorContrasenyas/Form1.Designer.cs
@@ -32,9 +32,18 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.inicioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.aLMACEN_CREDENCIALESDataSet = new GestorContrasenyas.ALMACEN_CREDENCIALESDataSet();
+            this.cREDENCIALESBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.cREDENCIALESTableAdapter = new GestorContrasenyas.ALMACEN_CREDENCIALESDataSetTableAdapters.CREDENCIALESTableAdapter();
+            this.idCredencialDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nombreUsuarioDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.contraseenyaDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.uRLDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.menuStrip1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.aLMACEN_CREDENCIALESDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cREDENCIALESBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -44,7 +53,8 @@
             this.inicioToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(926, 28);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(452, 28);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -52,20 +62,78 @@
             // 
             this.inicioToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("inicioToolStripMenuItem.Image")));
             this.inicioToolStripMenuItem.Name = "inicioToolStripMenuItem";
-            this.inicioToolStripMenuItem.Size = new System.Drawing.Size(34, 24);
+            this.inicioToolStripMenuItem.Size = new System.Drawing.Size(32, 24);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idCredencialDataGridViewTextBoxColumn,
+            this.nombreUsuarioDataGridViewTextBoxColumn,
+            this.contraseenyaDataGridViewTextBoxColumn,
+            this.uRLDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.cREDENCIALESBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(4, 31);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(443, 234);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // aLMACEN_CREDENCIALESDataSet
+            // 
+            this.aLMACEN_CREDENCIALESDataSet.DataSetName = "ALMACEN_CREDENCIALESDataSet";
+            this.aLMACEN_CREDENCIALESDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // cREDENCIALESBindingSource
+            // 
+            this.cREDENCIALESBindingSource.DataMember = "CREDENCIALES";
+            this.cREDENCIALESBindingSource.DataSource = this.aLMACEN_CREDENCIALESDataSet;
+            // 
+            // cREDENCIALESTableAdapter
+            // 
+            this.cREDENCIALESTableAdapter.ClearBeforeFill = true;
+            // 
+            // idCredencialDataGridViewTextBoxColumn
+            // 
+            this.idCredencialDataGridViewTextBoxColumn.DataPropertyName = "IdCredencial";
+            this.idCredencialDataGridViewTextBoxColumn.HeaderText = "IdCredencial";
+            this.idCredencialDataGridViewTextBoxColumn.Name = "idCredencialDataGridViewTextBoxColumn";
+            // 
+            // nombreUsuarioDataGridViewTextBoxColumn
+            // 
+            this.nombreUsuarioDataGridViewTextBoxColumn.DataPropertyName = "nombreUsuario";
+            this.nombreUsuarioDataGridViewTextBoxColumn.HeaderText = "nombreUsuario";
+            this.nombreUsuarioDataGridViewTextBoxColumn.Name = "nombreUsuarioDataGridViewTextBoxColumn";
+            // 
+            // contraseenyaDataGridViewTextBoxColumn
+            // 
+            this.contraseenyaDataGridViewTextBoxColumn.DataPropertyName = "contraseenya";
+            this.contraseenyaDataGridViewTextBoxColumn.HeaderText = "contraseenya";
+            this.contraseenyaDataGridViewTextBoxColumn.Name = "contraseenyaDataGridViewTextBoxColumn";
+            // 
+            // uRLDataGridViewTextBoxColumn
+            // 
+            this.uRLDataGridViewTextBoxColumn.DataPropertyName = "URL";
+            this.uRLDataGridViewTextBoxColumn.HeaderText = "URL";
+            this.uRLDataGridViewTextBoxColumn.Name = "uRLDataGridViewTextBoxColumn";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(926, 554);
+            this.ClientSize = new System.Drawing.Size(452, 270);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.aLMACEN_CREDENCIALESDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cREDENCIALESBindingSource)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -75,7 +143,14 @@
 
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem inicioToolStripMenuItem;
-        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private ALMACEN_CREDENCIALESDataSet aLMACEN_CREDENCIALESDataSet;
+        private System.Windows.Forms.BindingSource cREDENCIALESBindingSource;
+        private ALMACEN_CREDENCIALESDataSetTableAdapters.CREDENCIALESTableAdapter cREDENCIALESTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idCredencialDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nombreUsuarioDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn contraseenyaDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn uRLDataGridViewTextBoxColumn;
     }
 }
 
--- GestorContrasenyas/GestorContrasenyas/Form1.cs
+++ GestorContrasenyas/GestorContrasenyas/Form1.cs
@@ -16,5 +16,12 @@ namespace GestorContrasenyas
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: esta línea de código carga datos en la tabla 'aLMACEN_CREDENCIALESDataSet.CREDENCIALES' Puede moverla o quitarla según sea necesario.
+            this.cREDENCIALESTableAdapter.Fill(this.aLMACEN_CREDENCIALESDataSet.CREDENCIALES);
+
+        }
     }
 }
