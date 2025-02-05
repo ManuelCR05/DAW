59e25f4de0fe6c6a76459c8f6379f5e5eb48cc10
Manuel Contreras reyes <manconrey@alu.edu.gva.es>
2025-02-05 15:56:42 +0100

    Creación de MisDatosController
    Creación del controlador que se encarga de requerir los datos para completar la información de un usuario

---
+++ 2ºDAW/DWES/Practica Integradora/PracticaIntegradora/PracticaIntegradora/Controllers/MisDatosController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PracticaIntegradora.Data;
+using PracticaIntegradora.Models;
+
+namespace PracticaIntegradora.Controllers
+{
+    [Authorize(Roles = "Usuario")]
+    public class MisDatosController : Controller
+    {
+        private readonly MvcTiendaContexto _context;
+        public MisDatosController(MvcTiendaContexto context)
+        {
+            _context = context;
+        }
+        // GET: MisDatos/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: MisDatos/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult>
+        Create([Bind("Id,Nombre,Email,Telefono,Direccion,Poblacion,CodigoPostal,Nif")] Cliente empleado)
+        {
+            // Asignar el Email del usuario actual
+            empleado.Email = User.Identity.Name;
+            if (ModelState.IsValid)
+            {
+                _context.Add(empleado);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index", "Home");
+            }
+            return View(empleado);
+        }
+    }
+}
