37ce9a6a72a52848ab52029985b329e51f4c2f39
Manuel Contreras reyes <manconrey@alu.edu.gva.es>
2025-02-16 19:57:37 +0100

    Modificación vista escaparate
    Modificaciones en la vista del escaparate para implantar más medidas de filtrado

--- 2ºDAW/DWES/Practica Integradora/PracticaIntegradora/PracticaIntegradora/Controllers/EscaparateController.cs
+++ 2ºDAW/DWES/Practica Integradora/PracticaIntegradora/PracticaIntegradora/Controllers/EscaparateController.cs
@@ -1,12 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.DotNet.Scaffolding.Shared.CodeModifier.CodeChange;
+using PracticaIntegradora.Data;
+using PracticaIntegradora.Models;
 
 namespace PracticaIntegradora.Controllers
 {
     public class EscaparateController : Controller
     {
-        public IActionResult Index()
+        private readonly MvcTiendaContexto _context;
+
+        public EscaparateController(MvcTiendaContexto context)
         {
-            return View();
+            _context = context;
+        }
+
+        public IActionResult Index(int? IdCategoria)
+        {
+            var viewProductosCategorias = new ViewEscaparate
+            {
+                Productos = _context.Productos.Where(producto => producto.Escaparate == true && 
+                                                    (!IdCategoria.HasValue || producto.Categoria.Id == IdCategoria)).ToList(),
+                Categorias = _context.Categorias.ToList()
+            };
+
+            //var productos = _context.Productos.Where(producto => producto.Escaparate == true);
+
+            return View(viewProductosCategorias);
         }
     }
 }
---
+++ 2ºDAW/DWES/Practica Integradora/PracticaIntegradora/PracticaIntegradora/Models/ViewEscaparate.cs
@@ -0,0 +1,8 @@
+﻿namespace PracticaIntegradora.Models
+{
+    public class ViewEscaparate
+    {
+        public List<Producto> Productos { get; set; }
+        public List<Categoria> Categorias { get; set; }
+    }
+}
